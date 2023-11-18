af8577ae5b331ed5a09ff0d11ae40c4aba13cbe4
anxjos <jonassouzafonseca@gmail.com>
2023-11-18 17:08:16 -0300

    Adicionando lista 3 ao repo

---
+++ src/componentes/listaCliente.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Component } from "react";
+import 'materialize-css/dist/css/materialize.min.css'
+import ItemGrid from "./itemGrid";
+import { Tipo } from "../model/tipo";
+
+
+type props = {
+    tema: string
+    seletorView: Function
+}
+
+export default function ListaCliente(props:props) {
+    
+        let estilo = `collection-item active ${props.tema}`
+        return (
+            <div className="collection">
+
+                <ItemGrid tema={props.tema} nome=' 1' tipo={Tipo.Clientes} id='' seletorView = {props.seletorView}/>
+                <ItemGrid tema={props.tema} nome=' 2' tipo={Tipo.Clientes} id='' seletorView = {props.seletorView}/>
+                <ItemGrid tema={props.tema} nome=' 3' tipo={Tipo.Clientes} id='' seletorView = {props.seletorView}/>
+                <ItemGrid tema={props.tema} nome=' 4' tipo={Tipo.Clientes} id='' seletorView = {props.seletorView}/>
+            </div>
+        )
+}
